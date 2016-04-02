585e5a5e0cf8cc174913bc9e9b904fd9581e24c0
smajors <sjm36@students.uwf.edu>
2016-04-02 12:37:23 -0500

    Fragment initial fragment

---
+++ app/src/main/java/com/majorscreations/kindergartenapp/MathQuestionActivity.java
@@ -0,0 +1,21 @@
+package com.majorscreations.kindergartenapp;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MathQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_question);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        ArrangeNumbersOneToFiveFragment gameArrange = new ArrangeNumbersOneToFiveFragment();
+        transaction.replace(android.R.id.content, gameArrange);
+        transaction.commit();
+    }
+}
