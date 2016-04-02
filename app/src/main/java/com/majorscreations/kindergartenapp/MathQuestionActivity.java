347d674c9fcc03cfb8bb58cf6b5a246f35afb92e
Jarred Brown <jaroldosae@gmail.com>
2016-04-02 15:29:25 -0500

    no message

--- app/src/main/java/com/majorscreations/kindergartenapp/MathQuestionActivity.java
+++ app/src/main/java/com/majorscreations/kindergartenapp/MathQuestionActivity.java
@@ -2,6 +2,8 @@ package com.majorscreations.kindergartenapp;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ public class MathQuestionActivity extends AppCompatActivity {
 
     int total, right, wrong;
 
+    final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
     // List of fragments handled for this app
 
     ArrangeNumbersOneToFiveFragment arrangeGameFragment;
@@ -21,11 +24,20 @@ public class MathQuestionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_question);
 
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("key5", right);
+        editor.putInt("key6", wrong);
+        editor.putInt("key7", total);
+        editor.commit();
+
+
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         arrangeGameFragment = new ArrangeNumbersOneToFiveFragment();
         transaction.replace(android.R.id.content, arrangeGameFragment);
         transaction.commit();
+
+
     }
 
 }
