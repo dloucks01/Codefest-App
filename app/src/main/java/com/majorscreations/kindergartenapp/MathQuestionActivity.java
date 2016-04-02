72e23ea774b427bcba946b4d06f056aeef7cd9ea
smajors <sjm36@students.uwf.edu>
2016-04-02 14:39:01 -0500

    Fragment 5 numbers game created and logic created

--- app/src/main/java/com/majorscreations/kindergartenapp/MathQuestionActivity.java
+++ app/src/main/java/com/majorscreations/kindergartenapp/MathQuestionActivity.java
@@ -2,10 +2,15 @@ package com.majorscreations.kindergartenapp;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MathQuestionActivity extends AppCompatActivity {
+    // List of fragments handled for this app
+
+    ArrangeNumbersOneToFiveFragment arrangeGameFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,9 @@ public class MathQuestionActivity extends AppCompatActivity {
 
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        ArrangeNumbersOneToFiveFragment gameArrange = new ArrangeNumbersOneToFiveFragment();
-        transaction.replace(android.R.id.content, gameArrange);
+        arrangeGameFragment = new ArrangeNumbersOneToFiveFragment();
+        transaction.replace(android.R.id.content, arrangeGameFragment);
         transaction.commit();
     }
+
 }
