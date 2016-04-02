13efbc09b60bae298dbbb55b1c3f46e5e448b8d8
Jarred Brown <jaroldosae@gmail.com>
2016-04-02 01:42:35 -0500

    no message

---
+++ app/src/main/java/com/majorscreations/kindergartenapp/Topics.java
@@ -0,0 +1,74 @@
+package com.majorscreations.kindergartenapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class Topics extends AppCompatActivity {
+
+    Integer[] Choices = {R.mipmap.math};
+    ImageView pic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topics);
+        GridView grid = (GridView)findViewById(R.id.gridView);
+
+        grid.setAdapter(new ImageAdapter(this));
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //switch based on location to open each activity.
+                switch (position) {
+                    case 0:
+
+                        break;
+                }
+            }
+        });
+
+
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context context;
+
+        public ImageAdapter(Context c){
+            context = c;
+        }
+
+        @Override
+        public int getCount() {
+            return Choices.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            pic = new ImageView(context);
+            pic.setImageResource(Choices[position]);
+            pic.setScaleType(ImageView.ScaleType.FIT_XY);
+            pic.setLayoutParams(new GridView.LayoutParams(330,300));
+            return pic;
+        }
+    }
+
+}
+
